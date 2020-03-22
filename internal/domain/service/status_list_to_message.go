40956b6e9eb814aa036066d09d7e4f651109884f
hajowieland <mail@wieland.tech>
2020-03-22 11:30:58 +0100

    Change Slack message to show only 50 most affected countries

--- internal/domain/service/status_list_to_message.go
+++ internal/domain/service/status_list_to_message.go
@@ -16,11 +16,16 @@ func NewStatusListToMessage() *StatusListToMessage {
 }
 
 func (s StatusListToMessage) Convert(statusList []model.Status) model.Message {
-	result := "```"
+	result := ":biohazard_sign: *|COVID-19|*\n"
+	result += "_50 Most affected countries:_\n"
+	result += "```"
 	result += fmt.Sprintf("|%20s|%10s|%7s|%10s|\n", "Country", "Confirmed", "Deaths", "Recovered")
 	result += fmt.Sprintf("|%20s|%10s|%7s|%10s|\n", "", "", "", "")
+	i := 0
 	for _, status := range statusList {
-		result += fmt.Sprintf("|%20s|%10d|%7d|%10d|\n", status.Country(), status.Confirmed(), status.Deaths(), status.Recovered())
+		if i++; i <= 50 {
+			result += fmt.Sprintf("|%20s|%10d|%7d|%10d|\n", status.Country(), status.Confirmed(), status.Deaths(), status.Recovered())
+		}
 	}
 	result += "```"
 	return model.NewMessage(result)
